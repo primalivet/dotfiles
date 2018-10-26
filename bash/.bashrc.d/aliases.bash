3a048aaabd38a3e0b4c6dafc7cfd194dc53b49d5
Primalivet <gustafholm1@gmail.com>
2018-10-26 14:57:09 +0200

    switching from alias to functions

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -1,12 +1,19 @@
-if [ -x /usr/bin/dircolors ]; then
-    alias ls='ls --color=auto'
-    alias dir='dir --color=auto'
-    alias grep='grep --color=auto'
-fi
+# using function instead of aliases as it let's us process arguments sent to a
+# command, somthing that's impossible with aliases. You can read more about
+# this in Tom Ryders post https://sanctum.geek.nz/arabesque/custom-commands/
 
-# view all files exept (. and ..) in list format with human readable sizes and
-# group directories first
-alias ld='ls -Alh --group-directories-first'
+# ls on steriods
+# -A : all files, including .dotfiles. but don't include . and ..
+# -l : list/long format
+# -h : sizes in human readable format
+# --colors=auto : colors from LS_COLORS enviroment variable
+ll() {
+    if [ -x /usr/bin/dircolors ]; then
+        command ls -Alh --color=auto "$@"
+    else
+        command ls -Alh "$@"
+    fi
+}
 
 # use neovim in place of vi and vim if available
 if command_exists nvim; then
