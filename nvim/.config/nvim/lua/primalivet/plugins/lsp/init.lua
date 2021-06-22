3fdc490a9a9f2d65750dda67794fce8887b7c4c4
Primalivet <gustafholm1@gmail.com>
2021-06-22 22:28:50 +0200

    golang support

--- nvim/.config/nvim/lua/primalivet/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/primalivet/plugins/lsp/init.lua
@@ -4,6 +4,7 @@ function M.init()
   require("primalivet.plugins.lsp.sumneko").init()
   require("primalivet.plugins.lsp.vimls").init()
   require("primalivet.plugins.lsp.tsserver").init()
+  require("primalivet.plugins.lsp.gopls").init()
   require("primalivet.plugins.lsp.diagnosticls").init()
 end
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -6,6 +6,7 @@ export PATH=/bin:$PATH
 export PATH=/usr/sbin:/usr/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
 export PATH=$HOME/.bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/go/bin:$PATH
