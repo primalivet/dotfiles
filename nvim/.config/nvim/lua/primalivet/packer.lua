46d0b6956fcf4e9c659744ea9f8b940782ac1bfc
Primalivet <gustaf.holn@icloud.com>
2021-10-11 20:40:19 +0200

    nvim config in lua

--- nvim/.config/nvim/lua/primalivet/packer.lua
+++ nvim/.config/nvim/lua/primalivet/packer.lua
@@ -4,52 +4,15 @@ local cmd = vim.api.nvim_command
 local fn = vim.fn
 
 local function startup_cb(use)
-    -- The plugin manager
     use {"wbthomason/packer.nvim"}
     use {"nvim-lua/plenary.nvim"}
-
-    -- .editorconfig project files
     use {"editorconfig/editorconfig-vim"}
-
-    -- -- Generate JSDoc from a function signature
-    -- use {"heavenshell/vim-jsdoc", run = "make install", ft = {"javascript", "javascript.jsx", "typescript"}}
-
-    use {
-        "junegunn/fzf",
-        config = function()
-            -- should run pre loading
-            vim.api.nvim_set_var("fzf_layout", {down = "50%"})
-            vim.api.nvim_set_var("fzf_preview_window", {})
-            vim.cmd "command! Dotfiles exe 'Files ~/Code/OSS/dotfiles'"
-        end
-    }
-    use {"junegunn/fzf.vim"}
-
-    -- Show function signature in a "floating window" when typeing
-    use {"ray-x/lsp_signature.nvim", requires = {"neovim/nvim-lspconfig"}}
-
     use {"tpope/vim-commentary"}
     use {"tpope/vim-surround"}
     use {"tpope/vim-repeat"}
     use {"tpope/vim-fugitive"}
-
-    use {
-        "sindrets/diffview.nvim",
-        config = function()
-            require "primalivet.plugins.diffview".init()
-        end
-    }
-
-    -- Live preview of markdown while typeing
-    use {"iamcco/markdown-preview.nvim", run = "cd app && yarn install", ft = {"markdown"}}
-
-    -- -- Automatically insert tags for html and jsx etc.
-    -- use {"windwp/nvim-ts-autotag"}
-
-    -- Filebrower (only adapter to vifm cli program)
-    use {"vifm/vifm.vim"}
-
-    -- Automatically insert pairs like () {} "" etc.
+    use {"heavenshell/vim-jsdoc", run = "make install", ft = {"javascript", "javascript.jsx", "typescript"}}
+    use {"windwp/nvim-ts-autotag"}
     use {
         "windwp/nvim-autopairs",
         requires = "nvim-treesitter/nvim-treesitter",
@@ -58,7 +21,17 @@ local function startup_cb(use)
         end
     }
 
-    -- Show git changes in the signcolumn, revert changes etc.
+    use {"ray-x/lsp_signature.nvim", requires = {"neovim/nvim-lspconfig"}}
+
+    use {
+        "sindrets/diffview.nvim",
+        config = function()
+            require "primalivet.plugins.diffview".init()
+        end
+    }
+
+    use {"iamcco/markdown-preview.nvim", run = "cd app && yarn install", ft = {"markdown"}}
+
     use {
         "lewis6991/gitsigns.nvim",
         requires = {"nvim-lua/plenary.nvim"},
@@ -93,7 +66,6 @@ local function startup_cb(use)
                 }
             }
 
-            -- Gitsign commands
             vim.cmd "command! GitsignsResetHunk exe 'Gitsigns reset_hunk'"
             vim.cmd "command! GitsignsNextHunk exe 'Gitsigns next_hunk'"
             vim.cmd "command! GitsignsPrevHunk exe 'Gitsigns prev_hunk'"
@@ -101,7 +73,6 @@ local function startup_cb(use)
         end
     }
 
-    -- Search and find of a lot of things
     use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
     use {
         "nvim-telescope/telescope.nvim",
@@ -111,7 +82,6 @@ local function startup_cb(use)
         end
     }
 
-    -- Better code syntax support
     use {
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate",
@@ -120,7 +90,6 @@ local function startup_cb(use)
         end
     }
 
-    -- This is a tool to "view" what syntax group is used on a particular row/column
     use {
         "nvim-treesitter/playground",
         requires = {{"nvim-treesitter/nvim-treesitter"}},
@@ -129,10 +98,8 @@ local function startup_cb(use)
         end
     }
 
-    -- Formatting
     use {"jose-elias-alvarez/null-ls.nvim", requires = {"nvim-lua/plenary.nvim", "neovim/nvim-lspconfig"}}
 
-    -- Completion
     use {
         "hrsh7th/nvim-cmp",
         requires = {
@@ -148,7 +115,6 @@ local function startup_cb(use)
         end
     }
 
-    -- Language Servers
     use {
         "neovim/nvim-lspconfig",
         config = function()
@@ -156,7 +122,6 @@ local function startup_cb(use)
         end
     }
 
-    -- Langauge server helpers, mainly for Typescript
     use {
         "jose-elias-alvarez/nvim-lsp-ts-utils",
         requires = {"nvim-lua/plenary.nvim", "neovim/nvim-lspconfig", "jose-elias-alvarez/null-ls.nvim"}
