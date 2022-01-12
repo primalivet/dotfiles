f7474050fd38685d7ad95c693734ea27e23b3cd8
Primalivet <gustaf.holn@icloud.com>
2022-01-12 10:11:49 +0100

    cleanup null-ls config

--- nvim/.config/nvim/lua/primalivet/packer.lua
+++ nvim/.config/nvim/lua/primalivet/packer.lua
@@ -257,35 +257,33 @@ function M.init()
             local null_ls = require "null-ls"
             local helpers = require "null-ls.helpers"
 
-            local eslint_options = {
-                condition = function(utils)
-                    return utils.root_has_file ".eslintrc" or utils.root_has_file(".eslintrc.json")
-                end
+            local opts = {
+                eslint = {
+                    condition = function(utils)
+                        return utils.root_has_file ".eslintrc" or utils.root_has_file(".eslintrc.json")
+                    end
+                }
             }
 
-            local prettier_options = {
-                condition = function(utils)
-                    return utils.root_has_file ".prettierrc" or utils.root_has_file ".prettierrc.json"
-                end
-            }
-
-            local source_luafmt = {
-                method = null_ls.methods.FORMATTING,
-                filetypes = {"lua"},
-                name = "luafmt",
-                generator = helpers.formatter_factory {
-                    command = "luafmt",
-                    args = {"--stdin"}, -- go with luafmt defaults, it does not support a per project config as for now.
-                    to_stdin = true
+            local sources = {
+                luafmt = {
+                    method = null_ls.methods.FORMATTING,
+                    filetypes = {"lua"},
+                    name = "luafmt",
+                    generator = helpers.formatter_factory {
+                        command = "luafmt",
+                        args = {"--stdin"}, -- go with luafmt defaults, it does not support a per project config as for now.
+                        to_stdin = true
+                    }
                 }
             }
 
             require "null-ls".setup {
                 debug = true,
                 sources = {
-                    null_ls.builtins.diagnostics.eslint_d.with(eslint_options),
-                    null_ls.builtins.formatting.eslint_d.with(eslint_options),
-                    source_luafmt
+                    null_ls.builtins.diagnostics.eslint_d.with(opts.eslint),
+                    null_ls.builtins.formatting.eslint_d.with(opts.eslint),
+                    sources.luafmt
                 },
                 on_attach = function(client)
                     if client.resolved_capabilities.document_formatting then
