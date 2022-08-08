c50cf897f3cfb17a1116907484b417a7a98df6e6
Primalivet <gustaf.holn@icloud.com>
2022-08-08 09:20:36 +0200

    update mappings and commands to lua

--- nvim/.config/nvim/after/plugin/commands.lua
+++ nvim/.config/nvim/after/plugin/commands.lua
@@ -1,21 +1,45 @@
 vim.cmd(":packadd cfilter") -- enable filter quickfix list
-vim.cmd("command! ReloadConfig lua reload_config()<CR>")
 
-vim.cmd([[
-  augroup PRIMA_FILETYPES
-    autocmd BufRead,BufNewFile *.json set filetype=jsonc
-  augroup END
-]])
+vim.api.nvim_create_user_command("ResetHunk", "Gitsigns reset_hunk", {})
+vim.api.nvim_create_user_command("PreviewHunk", "Gitsigns preview_hunk", {})
 
--- TODO: if have gitsigns
-vim.cmd("command! ResetHunk Gitsigns reset_hunk")
-vim.cmd("command! PreviewHunk Gitsigns preview_hunk")
+-- Autocommands
 
--- TODO: if have fzf
-vim.cmd([[
-  augroup UserFZF!
-    autocmd! FileType fzf
-    autocmd  FileType fzf set laststatus=0 noshowmode noruler nonumber norelativenumber
-      \| autocmd BufLeave <buffer> set laststatus=2 showmode ruler number relativenumber
-  augroup END
-]])
+local group_id = vim.api.nvim_create_augroup("PRIMALIVET", {})
+
+vim.api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  { group = group_id, pattern = "*.json", command = "set filetype=jsonc" }
+)
+
+-- When in FZF buffer, hide irrelavant stuff and show it again when the user
+-- leaves the fzf buffer
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = group_id,
+  pattern = "fzf",
+  callback = function()
+    local user_opts = {
+      laststatus = vim.opt.laststatus:get(),
+      showmode = vim.opt.showmode:get(),
+      ruler = vim.opt.ruler:get(),
+      number = vim.opt.number:get(),
+      relativenumber = vim.opt.relativenumber:get(),
+    }
+    vim.opt.laststatus = 0
+    vim.opt.showmode = false
+    vim.opt.ruler = false
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+    vim.api.nvim_create_autocmd({ "BufLeave" }, {
+      group = group_id,
+      buffer = 0,
+      callback = function()
+        vim.opt.laststatus = user_opts.laststatus
+        vim.opt.showmode = user_opts.showmode
+        vim.opt.ruler = user_opts.ruler
+        vim.opt.number = user_opts.number
+        vim.opt.relativenumber = user_opts.relativenumber
+      end,
+    })
+  end,
+})
--- nvim/.config/nvim/after/plugin/mappings.lua
+++ nvim/.config/nvim/after/plugin/mappings.lua
@@ -1,68 +1,68 @@
-local nvim_set_keymap = vim.api.nvim_set_keymap
 local default_opt = { noremap = true, silent = true }
 
 -- Go over wrapped lines
-nvim_set_keymap("n", "j", "gj", default_opt)
-nvim_set_keymap("n", "k", "gk", default_opt)
+vim.api.nvim_set_keymap("n", "j", "gj", default_opt)
+vim.api.nvim_set_keymap("n", "k", "gk", default_opt)
 
 -- Keep selection in visual mode when indenting
-nvim_set_keymap("v", "<", "<gv", default_opt)
-nvim_set_keymap("v", ">", ">gv", default_opt)
+vim.api.nvim_set_keymap("v", "<", "<gv", default_opt)
+vim.api.nvim_set_keymap("v", ">", ">gv", default_opt)
 
 -- Move lines in visual mode
-nvim_set_keymap("v", "<C-j>", ":m '>+1<CR>gv=gv", default_opt)
-nvim_set_keymap("v", "<C-k>", ":m '<-2<CR>gv=gv", default_opt)
+vim.api.nvim_set_keymap("v", "<C-j>", ":m '>+1<CR>gv=gv", default_opt)
+vim.api.nvim_set_keymap("v", "<C-k>", ":m '<-2<CR>gv=gv", default_opt)
 
 -- Copilot (default to Tab for completion)
-nvim_set_keymap("i", "<C-d>", "<Plug>(copilot-dismiss)", default_opt)
-nvim_set_keymap("i", "<C-[>", "<Plug>(copilot-previous)", default_opt)
-nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-next)", default_opt)
+vim.api.nvim_set_keymap("i", "<C-d>", "<Plug>(copilot-dismiss)", default_opt)
+vim.api.nvim_set_keymap("i", "<C-[>", "<Plug>(copilot-previous)", default_opt)
+vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-next)", default_opt)
 
 -- Previous
-nvim_set_keymap("n", "[C", ":cfirst<CR>", default_opt)
-nvim_set_keymap("n", "[L", ":lfirst<CR>", default_opt)
-nvim_set_keymap("n", "[c", ":cprevious<CR>", default_opt)
-nvim_set_keymap("n", "[d", ":lua vim.diagnostic.goto_prev()<CR>", default_opt)
-nvim_set_keymap("n", "[h", ':lua require("gitsigns.actions").prev_hunk()<CR>', default_opt)
-nvim_set_keymap("n", "[l", ":lprevious<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "[C", ":cfirst<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "[L", ":lfirst<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "[c", ":cprevious<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "[d", ":lua vim.diagnostic.goto_prev()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "[h", ':lua require("gitsigns.actions").prev_hunk()<CR>', default_opt)
+vim.api.nvim_set_keymap("n", "[l", ":lprevious<CR>", default_opt)
 
 -- Next
-nvim_set_keymap("n", "]C", ":clast<CR>", default_opt)
-nvim_set_keymap("n", "]L", ":llast<CR>", default_opt)
-nvim_set_keymap("n", "]c", ":cnext<CR>", default_opt)
-nvim_set_keymap("n", "]d", ":lua vim.diagnostic.goto_next()<CR>", default_opt)
-nvim_set_keymap("n", "]h", ':lua require("gitsigns.actions").next_hunk()<CR>', default_opt)
-nvim_set_keymap("n", "]l", ":lnext<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "]C", ":clast<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "]L", ":llast<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "]c", ":cnext<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "]d", ":lua vim.diagnostic.goto_next()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "]h", ':lua require("gitsigns.actions").next_hunk()<CR>', default_opt)
+vim.api.nvim_set_keymap("n", "]l", ":lnext<CR>", default_opt)
 
 -- Search
-nvim_set_keymap("n", "<leader>s/", ":Lines<CR>", default_opt)
-nvim_set_keymap("n", "<leader>sb", ":Buffers<CR>", default_opt)
-nvim_set_keymap("n", "<leader>sc", ":Commands<CR>", default_opt)
-nvim_set_keymap("n", "<leader>sf", ":Files<CR>", default_opt)
-nvim_set_keymap("n", "<leader>sg", ":GitFiles<CR>", default_opt)
-nvim_set_keymap("n", "<leader>sh", ":History<CR>", default_opt)
-nvim_set_keymap("n", "<leader>sl", ":Rg<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>s/", ":Lines<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>sb", ":Buffers<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>sc", ":Commands<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>sf", ":Files<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>sg", ":GitFiles<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>sh", ":History<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>sl", ":Rg<CR>", default_opt)
 
 -- Edit
-nvim_set_keymap("n", "<leader>ea", ":lua vim.lsp.buf.code_action()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>ef", ":lua vim.lsp.buf.formatting()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>er", ":lua vim.lsp.buf.rename()<CR>", default_opt)
-nvim_set_keymap("v", "<leader>es", ":'<,'>sort<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>ea", ":lua vim.lsp.buf.code_action()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>ef", ":lua vim.lsp.buf.formatting()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>er", ":lua vim.lsp.buf.rename()<CR>", default_opt)
+vim.api.nvim_set_keymap("v", "<leader>es", ":'<,'>sort<CR>", default_opt)
 
 -- Goto
-nvim_set_keymap("n", "<leader>gd", ":lua vim.lsp.buf.definition()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>gf", "gf", default_opt)
-nvim_set_keymap("n", "<leader>gh", ":lua vim.lsp.buf.hover()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>gi", ":lua vim.lsp.buf.implementation()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>gr", ":lua vim.lsp.buf.references()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>gs", ":lua vim.lsp.buf.signature_help()<CR>", default_opt)
-nvim_set_keymap("n", "<leader>gt", ":lua vim.lsp.buf.type_definition()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gd", ":lua vim.lsp.buf.definition()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gf", "gf", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gh", ":lua vim.lsp.buf.hover()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gi", ":lua vim.lsp.buf.implementation()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gr", ":lua vim.lsp.buf.references()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gs", ":lua vim.lsp.buf.signature_help()<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>gt", ":lua vim.lsp.buf.type_definition()<CR>", default_opt)
 
 -- Toggle
-nvim_set_keymap("n", "<leader>tl", ":set list!<CR>", default_opt)
-nvim_set_keymap("n", "<leader>tp", ":set invpaste<CR>", default_opt)
-nvim_set_keymap("n", "<leader>ts", ":nohlsearch<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>tl", ":set list!<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>tp", ":set invpaste<CR>", default_opt)
+vim.api.nvim_set_keymap("n", "<leader>ts", ":nohlsearch<CR>", default_opt)
 
+-- TODO: DAP keymaps
 -- dc  require('dap').continue()
 -- dsO require('dap').step_out() "Debug: Step out" },
 -- dso require('dap').step_over()
