fcac714b3bdbeef0eff27a2815df283d3904dc9c
Primalivet <gustaf.holn@icloud.com>
2022-09-07 23:20:36 +0200

    Hook for tree-sitter mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -192,12 +192,32 @@
 
 (use-package tree-sitter
   :straight t
-  :init
+  :config
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
   (global-tree-sitter-mode 1))
 
 (use-package tree-sitter-langs
   :straight t
   :requires (tree-sitter))
+
+(use-package typescript-mode
+  :straight t)
+
+(use-package lsp-mode
+  :straight t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  ;; (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (typescript-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :straight t
+  :after (lsp-mode)
+  :commands lsp-ui-mode)
 
 (use-package gruber-darker-theme
   :straight t
