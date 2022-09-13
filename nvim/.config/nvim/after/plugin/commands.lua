0cfd8b6a1dc6f6da7edd494ee64745c86234a0c3
Primalivet <gustaf.holn@icloud.com>
2022-09-13 14:18:57 +0200

    Manually trigger open buffers diagnostics

--- nvim/.config/nvim/after/plugin/commands.lua
+++ nvim/.config/nvim/after/plugin/commands.lua
@@ -3,6 +3,13 @@ vim.cmd(":packadd cfilter") -- enable filter quickfix list
 vim.api.nvim_create_user_command("ResetHunk", "Gitsigns reset_hunk", {})
 vim.api.nvim_create_user_command("PreviewHunk", "Gitsigns preview_hunk", {})
 
+vim.api.nvim_create_user_command("Diagnostics", function()
+  print("put in qf list")
+  vim.diagnostic.setqflist({
+    title = "Diagnostics"
+  })
+end, {})
+
 -- Autocommands
 
 local group = vim.api.nvim_create_augroup("PRIMALIVET", {})
