f5794e4d958817ab47e5d22266c2a0ca369e852a
Primalivet <gustaf.holn@icloud.com>
2022-10-03 17:31:01 +0200

    extract bok.nvim (and extras) to separate plugin

--- nvim/.config/nvim/lua/primalivet/init.lua
+++ nvim/.config/nvim/lua/primalivet/init.lua
@@ -8,7 +8,7 @@ opt.diffopt = "internal,filler,closeoff,vertical"
 opt.expandtab = true
 opt.fileformats = "unix"
 opt.fileignorecase = true
--- opt.fillchars = "vert:|"
+opt.fillchars = "vert:|"
 opt.grepformat:append("%f:%l:%c:%m,%f:%l:%m")
 opt.grepprg = "rg --vimgrep --no-heading --hidden"
 opt.hidden = true -- dont unload abandoned buffers, just hide em
@@ -28,7 +28,7 @@ opt.smartcase = true
 opt.smartindent = true
 opt.softtabstop = 2
 opt.swapfile = false
-opt.termguicolors = true
+opt.termguicolors = false
 opt.timeoutlen = 500 --timeout for mappings
 opt.undofile = true -- uses the default undodir "~/.local/share/nvim/undo
 opt.updatetime = 100 --updatetime for events
@@ -37,7 +37,5 @@ opt.wildmenu = true
 opt.wildmode = "lastused:list:full"
 opt.wrap = false
 
-vim.opt.background = "dark"
-vim.cmd("colorscheme bok")
-
+vim.cmd("colorscheme sixteen")
 require("primalivet.packer").init()
