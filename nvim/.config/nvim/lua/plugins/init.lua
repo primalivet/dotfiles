0cf491171c607fef6c49da3d972de50fcafb1bee
Primalivet <gustaf.holn@icloud.com>
2023-05-15 07:55:41 +0200

    dont run colorizer automatically

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,14 +5,7 @@ return {
   "tpope/vim-repeat",
   "tpope/vim-commentary",
   "uga-rosa/ccc.nvim",
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
-  },
+  "NvChad/nvim-colorizer.lua",
   {
     "rebelot/kanagawa.nvim",
     opts = {
