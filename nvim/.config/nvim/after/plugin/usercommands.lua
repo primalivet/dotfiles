3ed4d7c6973021f2b09b7ff3c484fadbe45f6f5b
Gustaf Holm <gustaf.holm@icloud.com>
2023-08-25 09:20:18 +0200

    correct mistake commands, should have been here a long time ago
    RIP "Not en editor command"

--- nvim/.config/nvim/after/plugin/usercommands.lua
+++ nvim/.config/nvim/after/plugin/usercommands.lua
@@ -1,5 +1,8 @@
 vim.cmd(":packadd cfilter") -- enable filter quickfix list
 
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("W", "w", {})
+
 vim.api.nvim_create_user_command("ResetHunk", "Gitsigns reset_hunk", {})
 vim.api.nvim_create_user_command("PreviewHunk", "Gitsigns preview_hunk", {})
 
