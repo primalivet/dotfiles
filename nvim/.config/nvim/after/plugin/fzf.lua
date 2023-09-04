859fe90946b8e89d57ed8bea3ebc2dd47a8dff04
Gustaf Holm <gustaf.holm@icloud.com>
2023-09-04 12:37:17 +0200

    keep fzf in regular vim dir structure

---
+++ nvim/.config/nvim/after/plugin/fzf.lua
@@ -0,0 +1,3 @@
+vim.g.fzf_layout = { down = "30%" }
+vim.g.fzf_preview_window = {}
+vim.g.fzf_action = { ["ctrl-x"] = "split", ["ctrl-v"] = "vsplit" }
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,28 +1,15 @@
 return {
+  { "junegunn/fzf", dependencies = "junegunn/fzf.vim" },
   "nvim-lua/plenary.nvim",
   "editorconfig/editorconfig-vim",
   "tpope/vim-surround",
   "tpope/vim-repeat",
   "tpope/vim-commentary",
-  "uga-rosa/ccc.nvim",
-  { "NvChad/nvim-colorizer.lua", opts = {} },
   {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-        }
-      end,
-      colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
-    },
+    "echasnovski/mini.nvim",
+    version = "*",
+    config = function()
+      require("mini.align").setup()
+    end,
   },
-  { dir = "~/Code/OSS/cabin.nvim", opts = {} },
 }
