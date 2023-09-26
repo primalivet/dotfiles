c1f80b7ba55dfc39ef3a4a451535ce21289fc879
Gustaf Holm <gustaf.holm@icloud.com>
2023-09-26 17:17:01 +0200

    update md ftplugin

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,3 +1,3 @@
-vim.opt_local.number = false -- show numbers
-vim.opt_local.relativenumber = false --relative to current row
-vim.opt_local.wrap = true -- dont wrap
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+vim.opt_local.wrap = true
