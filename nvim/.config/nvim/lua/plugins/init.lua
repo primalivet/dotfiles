37a40f893287c5e58026ade4d89300e031968dd4
Gustaf Holm <gustaf.holm@icloud.com>
2024-01-28 08:28:36 +0100

    update plugins

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -15,7 +15,7 @@ return {
     config = function()
       require("harpoon").setup({
         menu = {
-          borderchars = {"", "", "", "", "", "", "", ""},
+          borderchars = { "", "", "", "", "", "", "", "" },
         },
       })
     end,
@@ -27,7 +27,16 @@ return {
     dependencies = "nvim-treesitter/nvim-treesitter",
     cmd = "TSPlaygroundToggle",
   },
-  { "junegunn/fzf", dependencies = "junegunn/fzf.vim" },
+  {"junegunn/fzf", dependencies = "junegunn/fzf.vim" },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   dependencies = {
+  --     {
+  --       "nvim-telescope/telescope-fzf-native.nvim",
+  --       build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+  --     },
+  --   },
+  -- },
   {
     "echasnovski/mini.nvim",
     version = "*",
@@ -61,7 +70,30 @@ return {
       "hrsh7th/cmp-nvim-lsp-signature-help",
       "davidsierradz/cmp-conventionalcommits",
       "roobert/tailwindcss-colorizer-cmp.nvim",
-      "Exafunction/codeium.vim",
+    },
+  },
+
+  -- AI
+  "Exafunction/codeium.vim",
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "mistral", -- The default model to use.
+      display_mode = "split", -- The display mode. Can be "float" or "split".
+      show_prompt = false, -- Shows the Prompt submitted to Ollama.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+      init = function(options)
+        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+      end,
+      -- Function to initialize Ollama
+      command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
+      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+      -- This can also be a lua function returning a command string, with options as the input parameter.
+      -- The executed command must return a JSON object with { response, context }
+      -- (context property is optional).
+      list_models = "<omitted lua function>", -- Retrieves a list of model names
+      debug = false, -- Prints errors and the command which is run.
     },
   },
 }
