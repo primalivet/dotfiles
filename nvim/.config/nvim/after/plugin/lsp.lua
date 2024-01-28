4c386ecf1444281af49d7494efc6bf5ed1593a4e
Gustaf Holm <gustaf.holm@icloud.com>
2024-01-28 08:29:30 +0100

    ocaml lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -43,11 +43,15 @@ lsp_conf.yamlls.setup({
   },
 })
 
+lsp_conf.ocamllsp.setup({
+
+})
+
 -- Install servers
 mason.setup({})
 mason_lspconf.setup({
   -- Manual (not supported by Mason registery): eslint_d, prettierd, stylua, clang-format, commitlint
-  ensure_installed = { "tsserver", "clangd", "cssls", "jsonls", "yamlls", "html", "tailwindcss", "lua_ls", "prismals" },
+  ensure_installed = { "tsserver", "clangd", "cssls", "jsonls", "yamlls", "html", "tailwindcss", "lua_ls", "prismals"},
   handlers = {
     lsp_zero.default_setup,
   },
