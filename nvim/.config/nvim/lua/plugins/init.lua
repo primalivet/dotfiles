1548acad287ff4d9bb98b46c5c18d4142b213396
Gustaf Holm <gustaf.holm@icloud.com>
2024-03-05 09:05:09 +0100

    chore: less files

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,20 +5,37 @@ vim.g.codeium_manual = true
 return {
   -- Misc
   "tpope/vim-fugitive",
-  "lewis6991/gitsigns.nvim",
+  {
+    "lewis6991/gitsigns.nvim", opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "±" },
+        untracked = { text = "" },
+      },
+    }
+  },
   "nvim-lua/plenary.nvim",
   -- Treesitter
-  "nvim-treesitter/nvim-treesitter",
+  { "nvim-treesitter/nvim-treesitter", config = function()
+    require("nvim-treesitter.configs").setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true }
+    })
+  end },
   {
-    "nvim-treesitter/playground",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    cmd = "TSPlaygroundToggle",
+    "junegunn/fzf", dependencies = "junegunn/fzf.vim", config = function()
+      vim.g.fzf_layout = { down = "50%" }
+      vim.g.fzf_preview_window = {}
+    end
   },
-  {"junegunn/fzf", dependencies = "junegunn/fzf.vim" },
   { "echasnovski/mini.nvim", version = "*", config = function()
     require('mini.surround').setup()
     require('mini.comment').setup()
-  end  },
+  end },
   -- LSP
   {
     "VonHeikemen/lsp-zero.nvim",
@@ -26,7 +43,6 @@ return {
     lazy = false,
     config = false,
     dependencies = {
-      { "nvimtools/none-ls.nvim", commit = "bb680d752cec37949faca7a1f509e2fe67ab418a" },
       "b0o/schemastore.nvim",
       "neovim/nvim-lspconfig",
       "williamboman/mason.nvim",
@@ -40,6 +56,82 @@ return {
       "hrsh7th/cmp-path",
     },
   },
+  { "nvimtools/none-ls.nvim", commit = "bb680d752cec37949faca7a1f509e2fe67ab418a", config = function()
+    local null_ls = require('null-ls')
+    local root_has_file = function(file)
+      return function(utils)
+        local has_file = utils.root_has_file(file)
+        return has_file
+      end
+    end
+
+    local eslint_root_files = { ".eslintrc", ".eslintrc.cjs", ".eslintrc.js", ".eslintrc.json" }
+    local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
+    local stylua_root_files = { "stylua.toml", ".stylua.toml" }
+    local ocamlformat_root_files = { "ocamlformat", ".ocamlformat" }
+    local clang_format_root_files = { ".clang-format" }
+
+    local json_file_has_field = function(filename, fieldname)
+      return function(utils)
+        if not utils.root_has_file(filename) then return false end
+        local contents = vim.fn.readfile(vim.fn.getcwd() .. "/" .. filename)
+        return vim.fn.json_decode(contents)[fieldname] ~= nil
+      end
+    end
+
+    local opts = {
+      eslint_formatting = {
+        condition = function(utils)
+          local has_eslint = root_has_file(eslint_root_files)(utils)
+          local has_prettier = root_has_file(prettier_root_files)(utils)
+          local has_package_json_eslint = json_file_has_field("package.json", "eslintConfig")(utils)
+          local has_package_json_prettier = json_file_has_field("package.json", "prettier")(utils)
+          local has_package_json_config = has_package_json_eslint and not has_package_json_prettier
+          return (has_eslint and not has_prettier) or (has_package_json_config)
+        end,
+      },
+      eslint_diagnostics = {
+        condition = root_has_file(eslint_root_files),
+      },
+      prettier_formatting = {
+        condition = function(utils)
+          local has_package_json_prettier = json_file_has_field("package.json", "prettier")(utils)
+          return root_has_file(prettier_root_files)(utils) or has_package_json_prettier
+        end,
+      },
+      stylua_formatting = {
+        condition = root_has_file(stylua_root_files),
+      },
+      ocamlformat_formatting = {
+        condition = root_has_file(ocamlformat_root_files),
+      },
+      clang_format_formatting = {
+        condition = root_has_file(clang_format_root_files),
+      },
+    }
+
+    local function on_attach(client, _)
+      if client.server_capabilities.document_formatting then
+        vim.cmd("command! -buffer Formatting lua vim.lsp.buf.formatting()")
+        vim.cmd("command! -buffer FormattingSync lua vim.lsp.buf.formatting_sync()")
+      end
+    end
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.eslint.with(opts.eslint_diagnostics),
+        null_ls.builtins.formatting.eslint.with(opts.eslint_formatting),
+        null_ls.builtins.formatting.prettier.with(opts.prettier_formatting),
+        null_ls.builtins.formatting.stylua.with(opts.stylua_formatting),
+        null_ls.builtins.formatting.ocamlformat.with(opts.ocamlformat_formatting),
+        null_ls.builtins.formatting.clang_format.with(opts.clang_format_formatting),
+        null_ls.builtins.code_actions.eslint.with(opts.eslint_diagnostics),
+        null_ls.builtins.code_actions.gitsigns,
+      },
+      on_attach = on_attach,
+    })
+
+  end },
 
   -- AI
   "Exafunction/codeium.vim",
