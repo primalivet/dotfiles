919572bc1e991429a3ca931a86f7491801590eca
Gustaf Holm <gustaf.holm@icloud.com>
2024-04-08 11:21:53 +0200

    dont resize wezterm when increase/decrease font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,6 +6,7 @@ config.color_scheme = 'AdventureTime'
 config.font_size = 22.0
 config.enable_tab_bar = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
+config.adjust_window_size_when_changing_font_size = false
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.colors = {
   foreground = '#dadada',
