2a0beefd53d3296fb784fd3c59fd3fc9595d958a
Gustaf Holm <gustaf.holm@icloud.com>
2024-05-29 19:40:16 +0200

    dark mode and builtin for wez

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,43 +1,61 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
+end
+
+local function color_scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "Builtin Tango Dark"
+  else
+    return "Builtin Tango Light"
+  end
+end
 
 local config = wezterm.config_builder()
-config.font = wezterm.font 'GitLab Mono'
-config.color_scheme = 'AdventureTime'
+config.font = wezterm.font("GitLab Mono", { weight = "Medium" })
+config.color_scheme = "AdventureTime"
 config.font_size = 22.0
 config.enable_tab_bar = false
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.adjust_window_size_when_changing_font_size = false
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-config.colors = {
-  foreground = '#dadada',
-  background = '#111111',
-  cursor_bg = '#ffd800',
-  cursor_fg = '#000000',
-  cursor_border = '#ffd800',
-  selection_fg = 'black',
-  selection_bg = 'white',
-  -- scrollbar_thumb = '#222222', -- TODO
-  -- split = '#444444', -- TODO
-  ansi = {
-    '#111111', -- Black
-    '#fd5e00', -- Red
-    '#00875f', -- Green
-    '#ffd800', -- Yellow
-    '#005faf', -- Blue
-    '#870087', -- Magenta
-    '#00605f', -- Cyan
-    '#9e9e9e' -- White
-  },
-  brights = {
-   '#3a3a3a', -- Bright black
-   '#ff875f', -- Bright red
-   '#01af87', -- Bright green
-   '#ffff5f', -- Bright yellow
-   '#01afff', -- Bright blue
-   '#875faf', -- Bright magenta
-   '#008787', -- Bright cyan
-   '#dadada', -- Bright white
-  }
-}
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.color_scheme = color_scheme_for_appearance(get_appearance())
+
+-- Old scheme
+-- config.colors = {
+--   foreground = "#dadada",
+--   background = "#111111",
+--   cursor_bg = "#ffd800",
+--   cursor_fg = "#000000",
+--   cursor_border = "#ffd800",
+--   selection_fg = "black",
+--   selection_bg = "white",
+--   -- scrollbar_thumb = '#222222', -- TODO
+--   -- split = '#444444', -- TODO
+--   ansi = {
+--     "#111111", -- Black
+--     "#fd5e00", -- Red
+--     "#00875f", -- Green
+--     "#ffd800", -- Yellow
+--     "#005faf", -- Blue
+--     "#870087", -- Magenta
+--     "#00605f", -- Cyan
+--     "#9e9e9e", -- White
+--   },
+--   brights = {
+--     "#3a3a3a", -- Bright black
+--     "#ff875f", -- Bright red
+--     "#01af87", -- Bright green
+--     "#ffff5f", -- Bright yellow
+--     "#01afff", -- Bright blue
+--     "#875faf", -- Bright magenta
+--     "#008787", -- Bright cyan
+--     "#dadada", -- Bright white
+--   },
+-- }
 
 return config
