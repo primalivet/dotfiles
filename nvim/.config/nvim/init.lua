9c064515161e4932c9147c51c966a504e42883eb
Gustaf Holm <gustaf.holm@icloud.com>
2024-12-20 09:54:00 +0100

    update neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
-local now, add = require'user'.setup()
+local now, add = require 'user'.setup()
 
 -- Options
 --------------------------------------------------------------------------------
@@ -13,8 +13,8 @@ vim.opt.smartcase = true
 vim.opt.ignorecase = true
 vim.opt.swapfile = false
 vim.opt.undofile = true
-vim.opt.expandtab = true 
-vim.opt.softtabstop = 2 
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 
@@ -23,10 +23,10 @@ vim.opt.smartindent = true
 
 local group = vim.api.nvim_create_augroup("USER", {})
 
-vim.api.nvim_create_autocmd({"TextYankPost"}, {
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   group = group,
   pattern = "*",
-  callback = function() vim.highlight.on_yank() end
+  callback = function() vim.hl.on_yank() end
 })
 
 -- Keymaps
@@ -34,23 +34,18 @@ vim.api.nvim_create_autocmd({"TextYankPost"}, {
 
 vim.g.mapleader = " "
 
--- Keep visual selection while indenting
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-
--- Center cursor on search jump
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
-
--- Add buffer diagnostics to location list
-vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist)
+vim.keymap.set("v", "<", "<gv", { desc = "Keep visual selection while indenting left" })
+vim.keymap.set("v", ">", ">gv", { desc = "Keep visual selection while indenting right" })
+vim.keymap.set("n", "n", "nzz", { desc = "Center cursor on search jump natural direction" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Center cursor on search jump unnatural direction" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Add buffer diagnostics to location list"})
 
 -- Syntax
 --------------------------------------------------------------------------------
 
-now(function() 
+now(function()
   add({ source = 'nvim-treesitter/nvim-treesitter' })
-  require"nvim-treesitter.configs".setup {
+  require "nvim-treesitter.configs".setup {
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true }
@@ -61,33 +56,41 @@ end)
 -- Language servers
 --------------------------------------------------------------------------------
 
-now(function() 
+now(function()
   add({ source = 'neovim/nvim-lspconfig' })
-  local lspconfig_util = require'lspconfig.util'
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  require"lspconfig".clangd.setup{ capabilities = capabilities }
-  require"lspconfig".ts_ls.setup{ 
-    capabilities = capabilities, 
-    root_dir = lspconfig_util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json'), 
-    single_file_support = false,
-  }
-  require"lspconfig".prismals.setup{ capabilities = capabilities }
-  require"lspconfig".eslint.setup{ capabilities = capabilities }
-  require"lspconfig".cssls.setup{ capabilities = capabilities }
-  require"lspconfig".html.setup{ capabilities = capabilities }
-  require"lspconfig".jsonls.setup{ capabilities = capabilities }
-  require"lspconfig".gopls.setup{ capabilities = capabilities }
-  require'lspconfig'.denols.setup{ 
-    capabilities = capabilities,
-    root_dir = lspconfig_util.root_pattern('deno.jsonc', 'deno.json'), 
-    single_file_support = false
+  local servers = {
+    clangd = {},
+    prismals = {},
+    eslint = {},
+    cssls = {},
+    html = {},
+    jsonls = {},
+    gopls = {},
+    lua_ls = { settings = { Lua = {} } },
+    denols = {
+      root_dir = function(fname)
+        return vim.fs.root(fname, {'deno.jsonc', 'deno.json'})
+      end,
+      single_file_support = false
+    },
+    ts_ls = {
+      root_dir = function(fname)
+        return vim.fs.root(fname, { 'tsconfig.json', 'jsconfig.json', 'package.json'})
+      end,
+      single_file_support = false
+    }
   }
 
+  for name, settings in pairs(servers) do
+    local combines = vim.tbl_extend('force', { capabilities = capabilities }, settings)
+    require'lspconfig'[name].setup(combines)
+  end
 
-  vim.api.nvim_create_autocmd({"LspAttach"}, {
+  vim.api.nvim_create_autocmd({ "LspAttach" }, {
     group = group,
     pattern = "*",
-    callback = function() 
+    callback = function()
       -- HINT: the <leader>gq mappings below is here rather then general since
       -- the neovim sets the 'formatexpr' to vim.lsp.formatexpr() on LspAttach.
       -- However, since most ts/js projects use prettier (instead of typescript
@@ -100,43 +103,51 @@ now(function()
       -- autocmd LspAttach * nnoremap <buffer> <leader>gq mf:%normal! gggqG<CR>'f
       -- autocmd LspAttach *.ts,*.tsx,*.js,*.jsx nnoremap <buffer> <leader>gq mf:%!./node_modules/.bin/prettier --stdin-filepath %<CR>'f
 
-      vim.keymap.set("n", "grn", vim.lsp.buf.rename)
-      vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
-      vim.keymap.set("n", "grr", vim.lsp.buf.references)
-      vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help)
+      vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+      vim.keymap.set("n", "gra", vim.lsp.buf.code_action, { desc = "List line code actions" })
+      vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "List symbol references" })
+      vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, { desc = "Open signature help" })
     end
   })
-
 end)
 
 -- Search
 --------------------------------------------------------------------------------
 
 now(function()
-  add({ 
-    source = "nvim-telescope/telescope.nvim", 
-    checkout = "0.1.8", 
+  add({
+    source = "nvim-telescope/telescope.nvim",
+    checkout = "0.1.8",
     depends = {
-      "nvim-lua/plenary.nvim", 
-      { 
-        source = "nvim-telescope/telescope-fzf-native.nvim", 
-        hooks = { 
-          post_install = function(ctx) 
-            vim.system({'make'}, { cwd = ctx.path }) 
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+      {
+        source = "nvim-telescope/telescope-fzf-native.nvim",
+        hooks = {
+          post_install = function(ctx)
+            vim.system({ 'make' }, { cwd = ctx.path })
           end
         }
       }
-    } 
+    }
   })
-  require'telescope'.setup {
-    defaults = {
-      preview = false,
-    },
-    extensions = { fzf = {} }
-  }
-  require('telescope').load_extension('fzf')
+  local telescope = require 'telescope'
+  local builtin = require 'telescope.builtin'
+  local themes = require 'telescope.themes'
 
-  vim.keymap.set("n", "<leader>sf", require'telescope.builtin'.find_files)
+  telescope.setup {
+    defaults = { preview = false },
+    pickers = {
+      find_files = { theme = "ivy" },
+      live_grep = { theme = "ivy" }
+    },
+    extensions = { fzf = {}, ['ui-select'] = { themes.get_ivy {} } }
+  }
+  pcall(telescope.load_extension, 'fzf')
+  pcall(telescope.load_extension, 'ui-select')
+
+  vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Find files" })
+  vim.keymap.set("n", "<leader>sl", builtin.live_grep, { desc = "Grep in files" })
 end)
 
 -- Git
@@ -145,10 +156,9 @@ end)
 now(function()
   add({ source = "tpope/vim-fugitive" })
   add({ source = "lewis6991/gitsigns.nvim" })
-  require'gitsigns'.setup{}
-  vim.keymap.set("n", "]h", require'gitsigns'.next_hunk)
-  vim.keymap.set("n", "[h", require'gitsigns'.prev_hunk)
-
+  require 'gitsigns'.setup {}
+  vim.keymap.set("n", "]h", require 'gitsigns'.next_hunk, { desc = "Go to next git hunk" })
+  vim.keymap.set("n", "[h", require 'gitsigns'.prev_hunk, { desc = "Go to previous git hunk" })
 end)
 
 -- AI
@@ -156,7 +166,7 @@ end)
 
 now(function()
   add({ source = "zbirenbaum/copilot.lua" })
-  require"copilot".setup {
+  require "copilot".setup {
     suggestion = { enable = false },
     panel = { enable = true, auto_refresh = true }
   }
@@ -167,4 +177,7 @@ end)
 
 -- Adapt theme to terminal foreground/background
 vim.api.nvim_set_hl(0, "Normal", { fg = "#D1D3D6", bg = "#1A1D21" })
-
+vim.api.nvim_set_hl(0, "DiffAdd", { fg = "#cbf9cb", bg = "#074008" })
+vim.api.nvim_set_hl(0, "DiffChange", { fg = "#cbf9f1", bg = "#074037" })
+vim.api.nvim_set_hl(0, "DiffText", { fg = "#cbf9f1", bg = "#0b6456" })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg = "#f9cbcb", bg = "#400707" })
