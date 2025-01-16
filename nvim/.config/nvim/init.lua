447d09ae95340417e4a377bbfd84b4cd3a2fc169
Gustaf Holm <gustaf.holm@icloud.com>
2025-01-16 07:52:18 +0100

    change diagnostic signals

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -258,10 +258,10 @@ vim.diagnostic.config{
   float = { border = "shadow", source = true },
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = "◉",
-      [vim.diagnostic.severity.WARN] = "◎",
-      [vim.diagnostic.severity.INFO] = "◯",
-      [vim.diagnostic.severity.HINT] = "◌",
+      [vim.diagnostic.severity.ERROR] = "●",
+      [vim.diagnostic.severity.WARN] = "◉",
+      [vim.diagnostic.severity.INFO] = "◎",
+      [vim.diagnostic.severity.HINT] = "◯",
     }
   },
 }
