8a3f933d939ca7288d73bf1daf4820f72e945dec
Gustaf Holm <gustaf.holm@icloud.com>
2025-02-01 16:16:29 +0100

    move specific devshells to main flake

--- flake.nix
+++ flake.nix
@@ -5,16 +5,23 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, ... }:
   let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs { 
+        inherit system; 
+        config.allowUnfree = true; 
+    };
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         with pkgs; [ 
 	  ripgrep
+          fd
 	  stow
 	  vim
           curl
@@ -23,7 +30,7 @@
           git
           jq
           lua-language-server
-          neovim
+          inputs.neovim-nightly-overlay.packages.aarch64-darwin.default
           nodejs_23
           tree
           typescript-language-server
@@ -80,6 +87,27 @@
     # $ darwin-rebuild build --flake .#papabear
     darwinConfigurations."papabear" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
+    };
+
+    devShells.aarch64-darwin.hb = pkgs.mkShell {
+      buildInputs = with pkgs; [];
+
+      shellHook = ''
+        export NPM_CONFIG_USERCONFIG=~/.hb/.npmrc
+        source ~/.hb/.zsh_private
+      '';
+    };
+
+    devShells.aarch64-darwin.vce = pkgs.mkShell {
+      buildInputs = with pkgs; [ 
+        kubectl
+        nodejs_22
+      ];
+
+      shellHook = ''
+        export NPM_CONFIG_USERCONFIG=~/.vce/.npmrc
+        source ~/.vce/.zsh_private
+      '';
     };
   };
 }
