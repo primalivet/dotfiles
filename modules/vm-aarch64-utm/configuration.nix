7dde72ba2eac20c8ea663cea89f43a4781a990c9
Gustaf Holm <gustafholm1@gmail.com>
2025-02-10 08:16:19 +0000

    update packages

--- modules/vm-aarch64-utm/configuration.nix
+++ modules/vm-aarch64-utm/configuration.nix
@@ -77,11 +77,10 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
+    curl
     wget
     firefox
     brave
-    tree
-    neovim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
