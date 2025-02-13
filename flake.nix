5da764b6480f2529c73535bf35044c2a28d4b486
Gustaf Holm <gustafholm1@gmail.com>
2025-02-13 07:54:13 +0100

    add option to rename conflicting files instead of exit and error

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.users.${user} = import ./users/${user}/home.nix;
             }
           ];
