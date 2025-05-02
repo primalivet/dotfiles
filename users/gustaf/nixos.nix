4d744da04f09af6da2e5051a75bf9f664f1b653c
Gustaf Holm <gustafholm1@gmail.com>
2025-05-02 14:05:55 +0200

    add docker

--- users/gustaf/nixos.nix
+++ users/gustaf/nixos.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
 {
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
+
   users.users.gustaf = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
     hashedPassword = "$6$.TJ/b9UC4jo3TOvz$DqfuGS5oj6O.X/zyHRRP9pjHJ9MbqcOHvfjSZbOFoBgMOE6dBvwXCbdqG0qkX2tC27pFZ0Hzgbics5TH2XDmU/";
     packages = with pkgs; [
       tree
