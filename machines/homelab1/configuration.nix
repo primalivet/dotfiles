45d6fb50c5d4dbe25a10c51e3d4c8209495f833c
Gustaf Holm <gustafholm1@gmail.com>
2025-05-07 11:28:26 +0200

    enable basic port for k3s single node cluster

--- machines/homelab1/configuration.nix
+++ machines/homelab1/configuration.nix
@@ -24,7 +24,17 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  networking.firewall.enable = true;
+  networking.firewall = {
+   enable = true;
+   allowedTCPPorts = [
+     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+     # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+   ];
+   allowedUDPPorts = [
+     # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+   ];
+  };
 
   networking.interfaces.enp0s1.ipv4.addresses = [
    {
