414bc9704d8e3213f3fad7f1cb006af10144c109
Gustaf Holm <gustafholm1@gmail.com>
2025-05-07 11:33:10 +0200

    enable k3s single node cluster

--- machines/homelab1/configuration.nix
+++ machines/homelab1/configuration.nix
@@ -64,6 +64,11 @@
   services.spice-vdagentd.enable = true;
   services.spice-autorandr.enable = true;
 
+  services.k3s = {
+    enable = true;
+    role = "server";
+  };
+
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
