63c4e7ad22a6818e5232c806cee11cce2c63db32
Gustaf Holm <gustafholm1@gmail.com>
2025-05-13 09:09:28 +0200

    add git

--- machines/macbook-pro/configuration.nix
+++ machines/macbook-pro/configuration.nix
@@ -4,6 +4,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    git
     curl
     neovim
     nodejs_23
