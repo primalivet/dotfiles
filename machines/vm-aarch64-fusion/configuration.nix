1df44d49feb9469adc38d1db0a6c0bacd543f388
Gustaf Holm <gustafholm1@gmail.com>
2025-08-08 09:21:23 +0200

    update vm-aarch64-fusion machine

--- machines/vm-aarch64-fusion/configuration.nix
+++ machines/vm-aarch64-fusion/configuration.nix
@@ -1,16 +1,11 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -19,90 +14,43 @@
     experimental-features = nix-command flakes
   '';
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
   time.timeZone = "Europe/Stockholm";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocales = [ "sv_SE.UTF-8/UTF-8" ];
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
   fonts.packages = with pkgs; [
+    terminus_font_ttf
     proggyfonts
   ];
 
   hardware.graphics.enable = true;
   virtualisation.vmware.guest.enable = true;
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Realtime Kit (handle realtime scheduling prio to user processes).
-  # Needed as PipeWire (and PulseAudio) uses this to have realtime playback.
   security.rtkit.enable = true; 
 
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
     enable = true;
-    systemWide = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
+  };
+
+  users.users.gustaf = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" "video" "realtime" ];
+    hashedPassword = "$6$OWAIYB4NAWgGyXXg$/IJsv3w4kNOq9Py2O9ZEQgk4q3.yUubYLDuon58ONS7mH4JmpoSWw8L668wVSEhV0QuMwSkPFir9Vyeor3V0Y.";
+    shell = pkgs.bash;
   };
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.gustaf = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    hashedPassword = "$6$OWAIYB4NAWgGyXXg$/IJsv3w4kNOq9Py2O9ZEQgk4q3.yUubYLDuon58ONS7mH4JmpoSWw8L668wVSEhV0QuMwSkPFir9Vyeor3V0Y.";
-    shell = pkgs.bashInteractive;
-  };
-
-  programs.sway = {
-    enable = true;
-  };
-
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };
+  programs.sway.enable = true;
 
   programs.firefox.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     curl
     foot
@@ -113,6 +61,8 @@
     grim # grab image from wl compositor
     i3status
     inxi # system information tool
+    libnotify
+    mako # notification deamon
     mesa-demos # demos and tests for Mesa/OpenGL
     mpv # video player
     nautilus
@@ -122,7 +72,7 @@
     st
     stow
     swayimg # image viewer
-    # TODO: mako # notification deamon
+    tmux
     tree
     vim 
     wf-recorder # screen recoding for wl compositor
@@ -130,29 +80,7 @@
     wl-clipboard
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
